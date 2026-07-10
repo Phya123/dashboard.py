bee0196c1c18773717d6a8916164de7f2ad4ed8f
Phya123 <90482389+Phya123@users.noreply.github.com>
2026-07-10 02:55:28 -0400

    Update performance.py

--- performance.py
+++ performance.py
@@ -1,6 +1,11 @@
 import os
-import json
 import pandas as pd
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+from alpaca.trading.client import TradingClient
+from alpaca.trading.requests import GetOrdersRequest
+from alpaca.trading.enums import QueryOrderStatus
+from alpaca.data.historical import StockHistoricalDataClient
 
 
 # ==========================
