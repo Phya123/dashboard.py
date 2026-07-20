e76d3ac7b273368a885874ce84588e20423ea119
Phya123 <90482389+Phya123@users.noreply.github.com>
2026-07-19 21:11:13 -0400

    Update profile.py

--- onboarding/onboarding/profile.py
+++ onboarding/onboarding/profile.py
@@ -11,51 +11,47 @@ def load_profiles():
         return []
 
     try:
+
         with open(PROFILE_FILE, "r") as f:
             return json.load(f)
 
     except Exception:
+
         return []
 
 
-
-def save_profiles(profiles):
+def create_profile(
+    name,
+    username,
+    interests,
+    goals
+):
 
     os.makedirs(
         "data",
         exist_ok=True
     )
 
-    with open(PROFILE_FILE, "w") as f:
-        json.dump(
-            profiles,
-            f,
-            indent=4
-        )
-
-
-
-def create_profile(
-    name,
-    username,
-    interests=None,
-    goals=None
-):
-
     profiles = load_profiles()
 
     profile = {
         "name": name,
         "username": username,
-        "interests": interests or [],
-        "goals": goals or []
+        "interests": interests,
+        "goals": goals
     }
-
 
     profiles.append(profile)
 
-    save_profiles(
-        profiles
-    )
+    with open(
+        PROFILE_FILE,
+        "w"
+    ) as f:
+
+        json.dump(
+            profiles,
+            f,
+            indent=4
+        )
 
     return profile
